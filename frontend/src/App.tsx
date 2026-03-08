259e9dc8a6de14c95da65b0d96a77e337d7a22cc
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-03-09 01:08:00 +0900

    Initial commit: Setup with Rails and React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-3xl font-bold text-slate-800">Knotionへようこそ</h1>
+      <p className="text-slate-500">テスト</p>
+      <Button>テストボタン</Button>
+    </div>
+  )
+}
+
+export default App
