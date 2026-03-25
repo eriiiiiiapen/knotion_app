ef861a0aeaa44fb0b998267d2309201d71c1311a
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-03-26 00:31:19 +0900

    ナレッジ登録機構を作成

--- backend/app/models/knowledge_article.rb
+++ backend/app/models/knowledge_article.rb
@@ -1,6 +1,6 @@
 class KnowledgeArticle < ApplicationRecord
   belongs_to :company
-  belongs_to :task
+  has_many :tasks
 
   enum :status, { draft: 0, published: 1 }
   validates :title, presence: true
